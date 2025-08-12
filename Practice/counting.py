5bd333f901fc750cb1a3465fd0d78f840e948ee2
wynsumhi <gusdk7244@naver.com>
2025-08-13 01:10:42 +0900

    [List1-2] 리스트 | 카운팅 정렬

---
+++ Practice/counting.py
@@ -0,0 +1,17 @@
+## 카운팅 정렬
+
+'''
+[input]
+arr = [0, 4, 1, 3, 1, 2, 4, 1]
+
+[output]
+[0, 1, 1, 1, 2, 3, 4, 4]
+'''
+
+
+def counting_sort(input_arr, k):
+    pass
+
+
+arr = [0, 4, 1, 3, 1, 2, 4, 1]
+print('정렬 결과:', counting_sort(arr, 5))  # [0, 1, 1, 1, 2, 3, 4, 4]
