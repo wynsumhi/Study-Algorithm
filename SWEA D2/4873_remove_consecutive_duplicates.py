6a2a78986246cd8aab537feffab2dd6e79526e55
wynsumhi <gusdk7244@naver.com>
2025-08-13 23:42:44 +0900

    [Stack1-1] 스택 | 괄호검사 | 파스칼의 삼각형

---
+++ SWEA D2/4873_remove_consecutive_duplicates.py
@@ -0,0 +1,14 @@
+## 4873. [파이썬 S/W 문제해결 기본] 4일차 - 반복문자 지우기
+
+'''
+[input]
+3
+ABCCB
+NNNASBBSNV
+UKJWHGGHNFTCRRCTWLALX
+
+[output]
+#1 1
+#2 4
+#3 11
+'''
---
+++ SWEA D2/bracket_check.py
@@ -0,0 +1,14 @@
+## 4866. [파이썬 S/W 문제해결 기본] 4일차 - 괄호검사
+
+'''
+[input]
+3
+print('{} {}'.format(1, 2))
+N, M = map(int, input().split())
+print('#{} {}'.format(tc, find())
+
+[output]
+#1 1
+#2 1
+#3 0
+'''
