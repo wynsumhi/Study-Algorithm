c924fc95d57b604112a27fc4e1b8b57259bcd1fa
wynsumhi <gusdk7244@naver.com>
2025-08-13 23:18:11 +0900

    [String1] 문자열 | 회문 | GNS

---
+++ Practice/str_reverse.py
@@ -0,0 +1,20 @@
+## 문자열 뒤집기 - 연습문제_1_문자열_뒤집기
+
+# 자기 문자열에서 뒤집는 방법이 있고 새로운 빈 문자열을 만들어 소스의 뒤에서 부터 읽어서 타겟에 쓰는 방법이 있겠다.
+# 자기 문자열을 이용할 경우는 swap을 위한 임시 변수가 필요하며 반복 수행을 문자열 길이의 반만 수행해야 한다.
+# Reverse 함수 혹은 slice notation 을 이용하여 구현
+
+'''
+[input]
+4
+algorithm
+life is short
+you need python
+SSAFY
+
+[output]
+#1 mhtirogla
+#2 trohs si efil
+#3 nohtyp deen uoy
+#4 YFASS
+'''
---
+++ SWEA D3/5356_say_column.py
@@ -0,0 +1,20 @@
+## 5356. 의석이의 세로로 말해요
+
+'''
+[input]
+2
+ABCDE
+abcde
+01234
+FGHIJ
+fghij
+AABCDD
+afzz
+09121
+a8EWg6
+P5h3kx
+
+[output]
+#1 Aa0FfBb1GgCc2HhDd3IiEe4Jj
+#2 Aa0aPAf985Bz1EhCz2W3D1gkD6x	//TC #1의 정답
+'''
