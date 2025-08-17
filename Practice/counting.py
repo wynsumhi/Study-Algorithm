226b3ee346b8c89dfa99986588a5d99f37b6c6bf
wynsumhi <gusdk7244@naver.com>
2025-08-17 17:01:33 +0900

    [List 1-2] 리스트 | 카운팅정렬

--- Practice/counting.py
+++ Practice/counting.py
@@ -7,11 +7,34 @@ arr = [0, 4, 1, 3, 1, 2, 4, 1]
 [output]
 [0, 1, 1, 1, 2, 3, 4, 4]
 '''
+# 1. 데이터 범위 크기(k+1)의 배열을 준비
+# 2. 각 숫자의 빈도(등장 횟수)를 기록
+# 3. 누적합으로 바꿔서 “이 숫자가 들어갈 자리” 계산
+# 4. 입력 배열을 뒤에서부터 순회하며 결과 배열에 채움
+# 5. 결과 반환
 
+# 0 ~ K 까지의 정수를 가진 배열 정렬
+def counting_sort(input_arr, k): # (주어진 배열, 가장 큰 수 K)
+    # 1. K + 1 크기의 카운팅 배열을 0으로 생성 (인덱스 0부터 K까지)
+    counting_arr = [0] * (k + 1) # 각 인덱스는 숫자 / 값은 그 숫자가 몇 번 나왔는지(빈도)
 
-def counting_sort(input_arr, k):
-    pass
+    # 2. 주어진 배열의 num을 순회하며 각 원소의 빈도수를 카운팅 배열에 기록
+    for num in input_arr:
+        counting_arr[num] += 1
 
+    # 3. 누적 합을 계산하여 각 원소가 정렬된 배열 내에서 차지할 위치 결정
+    for i in range(1, k + 1):
+        counting_arr[i] += counting_arr[i - 1]
+
+    # 결과 배열을 입력 배열과 같은 크기로 생성
+    result_arr = [0] * len(input_arr)
+
+    # 4. 입력배열을 역순으로 순회하며 결과 배열에 채움
+    for num in reversed(input_arr):
+        counting_arr[num] -= 1
+        result_arr[counting_arr[num]] = num
+
+    return result_arr
 
 arr = [0, 4, 1, 3, 1, 2, 4, 1]
 print('정렬 결과:', counting_sort(arr, 5))  # [0, 1, 1, 1, 2, 3, 4, 4]
--- SWEA D2/4834_num_card.py
+++ SWEA D2/4834_num_card.py
@@ -1,1 +1,54 @@
 ## 4834. [파이썬 S/W 문제해결 기본] 1일차 - 숫자 카드
+
+'''
+[input]
+3
+5
+49679
+5
+08271
+10
+7797946543
+
+[output]
+#1 9 2
+#2 8 1
+#3 7 3
+'''
+
+# 0 ~ 9 까지 적힌 숫자 N장의 카드 중
+# 가장 많은 카드에 적힌 숫자와 카드가 몇 장인지!
+## 카운트 정렬을 사용하여 가장 많은 카운트를 쌓은 카드의 숫자와 장수를 구한다.
+
+T = int(input())
+
+for tc in range(1, T + 1):
+    N = int(input())
+    cards = list(map(int, input()))
+
+    # 가장 많은 카드(찾는 카드의 숫자)
+    max_card = 0
+
+    # 가장 많은 카드의 장수
+    max_count = 0
+
+    # 카드 수를 저장하는 리스트(카운트 배열) - [0, 0, 0, 0...]
+    num_of_cards = [0] * 10
+
+    # 주어진 카드의 각각을 순회하며
+    for card in cards:
+        # 만약 임의 생성한 카운트 배열의 card 원소가 일치하면
+        num_of_cards[card] += 1
+
+    # 생성한 카운트 배열의 길이만큼 순회하며
+    for i in range(len(num_of_cards)):
+        # 카운트 배열의 인덱스에 쌓인 수를 가장 많은 카드의 장수로 누적
+        if max_count < num_of_cards[i]:
+            max_count = num_of_cards[i]
+            # 가장 많은 카드에 적힌 수 = 카운팅 배열의 인덱스
+            max_card = i
+        # ** 같은 개수라면 더 큰 숫자를 선택
+        elif num_of_cards[i] == max_count and i > max_card:
+            max_card = i
+
+    print(f'#{tc} {max_card} {max_count}')
