8b7fea21bca431e5435a1d1c7065e9320c46185d
HYUNA <100817058+wynsumhi@users.noreply.github.com>
2025-12-17 12:06:35 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,10 +1,7 @@
-arr = [1, 1, 2, 2, 2, 8]
+standard = [1, 1, 2, 2, 2, 8]
 
-find = list(map(int, input().split()))
+current = list(map(int, input().split()))
 
-result = []
-
-for i in range(6):
-    result.append(arr[i] - find[i])
+result = [stand - cur for stand, cur in zip(standard, current)]
 
 print(*result)
